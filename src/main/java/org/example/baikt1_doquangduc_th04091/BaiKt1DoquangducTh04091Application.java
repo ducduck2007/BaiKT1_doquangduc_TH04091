b1b89bcbe740400ad9acd6679ed0be3759b222f8
ducduck2007 <ducdoquang207@gmail.com>
2025-10-06 16:19:31 +0700

    .

---
+++ src/main/java/org/example/baikt1_doquangduc_th04091/BaiKt1DoquangducTh04091Application.java
@@ -0,0 +1,13 @@
+package org.example.baikt1_doquangduc_th04091;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiKt1DoquangducTh04091Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiKt1DoquangducTh04091Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/baikt1_doquangduc_th04091/BaiKt1DoquangducTh04091ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baikt1_doquangduc_th04091;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiKt1DoquangducTh04091ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
