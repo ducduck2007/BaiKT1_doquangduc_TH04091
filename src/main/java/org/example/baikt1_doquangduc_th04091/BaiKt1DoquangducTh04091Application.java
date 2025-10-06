fb6d7234b2258bb2f32724f6f022e82c9c87901d
ducduck2007 <ducdoquang207@gmail.com>
2025-10-06 16:35:49 +0700

    .

--- src/main/java/org/example/baikt1_doquangduc_th04091/BaiKt1DoquangducTh04091Application.java
+++ src/main/java/org/example/baikt1_doquangduc_th04091/BaiKt1DoquangducTh04091Application.java
@@ -8,6 +8,7 @@ public class BaiKt1DoquangducTh04091Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BaiKt1DoquangducTh04091Application.class, args);
+        System.out.println("running...");
     }
 
 }
